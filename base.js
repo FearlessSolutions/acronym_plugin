d882d6b4f3af4a96a85f3e4f83011f7a37b8aeaa
Kelly <kdyer@users.noreply.github.com>
2020-09-30 15:51:48 -0400

    Reorganize the file

--- base.js
+++ base.js
@@ -1,18 +1,18 @@
 //reference location for acronym files for the entire extension
 const fileReference = [
   {
-    ref: "default",
-    name: "Fearless (always included)",
-    default: true,
-    url:
-      "https://raw.githubusercontent.com/FearlessSolutions/acronym_plugin/master/acronyms/fearless.json",
-  },
-  {
     ref: "cms",
     name: "CMS Portfolio",
     default: false,
     url:
       "https://raw.githubusercontent.com/FearlessSolutions/acronym_plugin/master/acronyms/cms.json",
+  },
+  {
+    ref: "default",
+    name: "Fearless (always included)",
+    default: true,
+    url:
+      "https://raw.githubusercontent.com/FearlessSolutions/acronym_plugin/master/acronyms/fearless.json",
   },
   {
     ref: "software",
